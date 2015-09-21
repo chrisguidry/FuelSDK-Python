b3368761fd49e6bca7cd23e3f7bf2793b645ed48
Chris Guidry <cguidry@safaribooksonline.com>
2015-09-21 10:31:43 -0400

    Another attempt at Python3

--- FuelSDK/__init__.py
+++ FuelSDK/__init__.py
@@ -4,11 +4,11 @@ __version__ = '0.9.3'
 
 # Runtime patch the suds library
 from .suds_patch import _PropertyAppender
-from .suds.mx import appender as _appender
+from suds.mx import appender as _appender
 _appender.PropertyAppender = _PropertyAppender
 
 from .suds_patch import _bodycontent
-from .suds.bindings import document as _document
+from suds.bindings import document as _document
 _document.Document.bodycontent = _bodycontent
 # end runtime patching of suds
 
